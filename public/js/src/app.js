8c8ae71498195811e029b065c64ac818db02115d
Jason Rushton <github.com@schtuph.com>
2015-03-29 19:13:24 -0600

    remove lesshat, auto-prefix section headers with GitHub style anchor links

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -6,5 +6,21 @@ $(function() {
 		$(".navbar-toggle").click();
 	});
 
+
+	// prepend target links to headers
+	$('[id]:has(:header)').each((i, el) => {
+		let $el = $(el);
+		let id = $el.attr('id');
+
+		let $headers = $(':header', el);
+		let $firstHeader = $($headers[0]);
+		let $anchor = $(`<a href="#${id}"><i class="fa"></i></a>`);
+
+		$firstHeader
+			.addClass('target')
+			.prepend($anchor);
+	});
+
+
 	require('./nospam')('.nospam-email-address');
 });
