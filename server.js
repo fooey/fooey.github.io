c11479818408bb33abe5f61bd5edc54ed021f165
Jason Rushton <github.com@schtuph.com>
2015-04-10 17:50:09 -0600

    fix procfile, have to actually define entry xD

--- server.js
+++ server.js
@@ -21,24 +21,19 @@ require('./routes')(app, express);
 
 
 
-console.log('');
-console.log('**************************************************');
-console.log('Express server starting');
-console.log('Time: %d',     GLOBAL.appStartTime);
-console.log('Port: %d',     appPort);
-console.log('Mode: %s',     appEnv);
-console.log('PID: %s',      process.pid);
-console.log('Platform: %s', process.platform);
-console.log('Arch: %s',     process.arch);
-console.log('Node: %s',     process.versions.node);
-console.log('V8: %s',       process.versions.v8);
-console.log('**************************************************');
-console.log('');
-
 app.listen(appPort, function() {
   console.log('');
   console.log('**************************************************');
   console.log('Express server started');
+  console.log('Node service starting');
+  console.log('Time: %d',     GLOBAL.appStartTime);
+  console.log('Port: %d',     appPort);
+  console.log('Mode: %s',     appEnv);
+  console.log('PID: %s',      process.pid);
+  console.log('Platform: %s', process.platform);
+  console.log('Arch: %s',     process.arch);
+  console.log('Node: %s',     process.versions.node);
+  console.log('V8: %s',       process.versions.v8);
   console.log('**************************************************');
   console.log('');
 });
